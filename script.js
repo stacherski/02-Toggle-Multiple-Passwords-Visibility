7ae7d7bfaef746c0025371ad78991395d548c4a6
stacherski <stacherski@gmail.com>
2022-10-16 08:44:57 +0200

    master

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const togglePasswordVisibility = ELM => {
-    const FIELDS_PASSWORD = ELM.closest('form').querySelectorAll('[name*="password"]:not([name*="show"])')
-    ELM.checked ? FIELDS_PASSWORD.forEach(field => {field.type='text'}) : FIELDS_PASSWORD.forEach(field => {field.type='password'})
-}
+const togglePasswordVisibility = (ELM) => {
+  const FIELDS_PASSWORD = ELM.closest("form").querySelectorAll(
+    '[name*="password"]:not([name*="show"])'
+  );
+  ELM.checked
+    ? FIELDS_PASSWORD.forEach((field) => {
+        field.type = "text";
+      })
+    : FIELDS_PASSWORD.forEach((field) => {
+        field.type = "password";
+      });
+};
 
-document.addEventListener('click', EVT => {
-    EVT.target.matches('form input[type="checkbox"][id*="show-"]') ? togglePasswordVisibility(EVT.target) : ''
-})
+document.addEventListener("click", (EVT) => {
+  EVT.target.matches('form input[type="checkbox"][name*="show"]')
+    ? togglePasswordVisibility(EVT.target)
+    : "";
+});
