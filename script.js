8206783dc3b1c6c3b12efc8b735a78d4b6e90893
stacherski <stacherski@gmail.com>
2022-10-19 08:44:47 +0200

    master

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const CHARACTER_COUNT = document.querySelector("#character-count");
+const WORD_COUNT = document.querySelector("#word-count");
 const TEXTAREA = document.querySelector("#text");
 
 //for repeated code used with multiple events
 //provide array with event names
 //and use forEach to iterate through
-["keydown", "keyup"].forEach((EVT) => {
+["input"].forEach((EVT) => {
   TEXTAREA.addEventListener(EVT, () => {
     CHARACTER_COUNT.textContent = TEXTAREA.value.length;
+    let WORDS = TEXTAREA.value.replace(",", "").replace(".", "").split(" ");
+    WORD_COUNT.textContent = WORDS.length;
   });
 });
